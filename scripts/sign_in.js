98adf3f7a975dda7b8a73cba18fb5d93c3e96d3c
Maxwell Tung <max.tung.3@gmail.com>
2016-08-02 13:34:02 -0400

    started sign in verification, switched to angular

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -1,18 +1,42 @@
 /* sign_in.js */
 
-var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
 
-request.open("GET", "data/accounts.json", true);
+// request.open("GET", "data/accounts.json", true);
 
-function verify() {
-        var uName = document.getElementById("username_input");
-        var pWord = document.getElementById("password_input");
 
-        var accountData = request.responseText;
-        console.log(accountData);
-        var accounts = JSON.parse(accountData);
 
-        console.log(accounts[uName] == pWord);
-        console.log(uName.value);
-        console.log(pWord.value);
-}
+// function verify() {
+//         var uName = document.getElementById("username_input");
+//         var pWord = document.getElementById("password_input");
+
+//         var accountData = request.responseText;
+//         console.log(accountData);
+//         var accounts = JSON.parse(accountData);
+
+//         console.log(accounts[uName] == pWord);
+//         console.log(uName.value);
+//         console.log(pWord.value);
+// }
+
+var PortalApp = angular.module("Portal", []);
+
+PortalApp.controller("SignInController", function($scope) {
+        $scope.username = "";
+        $scope.password = "";
+        $scope.verify = function() {
+                console.log("username "+ $scope.username);
+                console.log("password "+ $scope.password);
+                if ($scope.username == "maxtung" && $scope.password == "secret") {
+                        console.log("signed in!");
+                } else {
+                        console.log("invalid!");
+                }
+        }
+        $scope.keypressCredentials = function(keyEvent) {
+                if (keyEvent.which === 13) {
+                        $scope.verify();
+                }
+        }
+
+});
