6f8d606a35b09a8f0981a4a3e8382c5338b8199e
mtung03 <max.tung.3@gmail.com>
2016-08-17 21:32:02 -0400

    add sanitize script

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -44,3 +44,31 @@ PortalApp.controller("AccountRequestController", function($scope, $http, $saniti
                 }
         }
 });
+
+PortalApp.controller("BuyerController", function($scope, $http, $sanitize) {
+        $scope.hideProjects = false;
+        $scope.hideMailbox  = true;
+        $scope.hideRatings  = true;
+        console.log("buying");
+
+        $scope.selectProjectTab = function() {
+                console.log("proj");
+                $scope.hideProjects = false;
+                $scope.hideMailbox  = true;
+                $scope.hideRatings  = true;
+        }
+
+        $scope.selectMailboxTab = function() {
+                console.log("mail");
+                $scope.hideProjects = true;
+                $scope.hideMailbox  = false;
+                $scope.hideRatings  = true;
+        }
+
+        $scope.selectRatingsTab = function() {
+                console.log("rate");
+                $scope.hideProjects = true;
+                $scope.hideMailbox  = true;
+                $scope.hideRatings  = false;
+        }
+});
