a538856e8897e77cbe6fa318d2506d3cb5f30473
mtung03 <max.tung.3@gmail.com>
2016-08-20 00:40:14 -0400

    fixed checkboxes to allow multiple choices of vendor

--- scripts/buyer.js
+++ scripts/buyer.js
@@ -56,8 +56,9 @@ PortalApp.controller("BuyerController", function($scope, $http, $sanitize) {
                 method: "GET",
                 url: "/data/vendors.json"
         }).then(function success(response) {
-                $scope.vendorOptions = response.data;
+                $scope.vendors = response.data;
                 console.log("got vendors");
+                $scope.vendorOptions = makeVendorOptions();
                 console.log($scope.vendorOptions);
         }, function error(response) {
                 console.log("could not find vendors");
@@ -65,7 +66,36 @@ PortalApp.controller("BuyerController", function($scope, $http, $sanitize) {
 
         $scope.projectName = "";
         $scope.description = "";
-        $scope.vendorChoices;
+
+        function makeVendorOptions() {
+            var options = [];
+            for (i = 0; i < $scope.vendors.length; i++) {
+                var option = $scope.vendors[i];
+                option["selected"] = false;
+                options.push(option);
+            }
+            return options;
+        };
+
+        console.log("after make" + $scope.vendorOptions);
+
+        // selected vendors
+        $scope.vendorChoices = [];
+
+        // helper method to get selected vendors
+        $scope.selectedVendors = function selectedVendors() {
+            return filterFilter($scope.vendorOptions, { selected: true });
+        };
+
+        // watch vendors for changes
+        $scope.$watch('vendorOptions|filter:{selected:true}', function (nv) {
+            $scope.vendorChoices = nv.map(function (vendor) {
+                return vendor.title;
+            });
+            console.log("ok");
+        }, true);
+
+        console.log($scope.vendorChoices);
 
         $scope.createProject = function() {
                 var projectData = "projectName=" + $sanitize($scope.projectName) + "&description=" + $sanitize($scope.description);
